8cc6e8481568f4b3f473844270dd13d4e115193f
Martin Atkins <matkins@saymedia.com>
2013-10-08 11:27:53 -0700

    Add some more elaborate example form controls.
    Includes a select box and a number field, as well as binding controls to
    fields in a sub-object.

--- example/example.js
+++ example/example.js
@@ -18,6 +18,45 @@
                      type: 'date'
                  },
                  {
+                     caption: 'Street Address',
+                     model: 'address.street',
+                     type: 'string',
+                     maxLength: 25
+                 },
+                 {
+                     caption: 'City',
+                     model: 'address.city',
+                     type: 'string',
+                     maxLength: 25
+                 },
+                 {
+                     caption: 'State',
+                     model: 'address.state',
+                     type: 'select',
+                     options: [
+                         {
+                             caption: 'California',
+                             value: 'CA'
+                         },
+                         {
+                             caption: 'New York',
+                             value: 'NY'
+                         },
+                         {
+                             caption: 'Washington',
+                             value: 'WA'
+                         }
+                     ]
+                 },
+                 {
+                     caption: 'Zip Code',
+                     model: 'address.zipCode',
+                     type: 'integer',
+                     maxLength: 5,
+                     minValue: 10000,
+                     maxValue: 99999
+                 },
+                 {
                      caption: 'Nicknames',
                      model: 'nicknames',
                      type: 'array<string>'
@@ -25,7 +64,8 @@
              ];
 
              $scope.data = {
-                 name: 'John Smith'
+                 name: 'John Smith',
+                 address: {}
              };
          }
      );
